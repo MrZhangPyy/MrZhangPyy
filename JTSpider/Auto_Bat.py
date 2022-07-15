b37b9bd6359cc61c9e7fa4f46b07e68b598bdcb9
MrZhang <mrzhang_py@163.com>
2022-07-15 22:14:25 +0800

    Package number checker and filter added.

--- JTSpider/Auto_Bat.py
+++ JTSpider/Auto_Bat.py
@@ -5,8 +5,11 @@ import time
 from datetime import datetime
 import numpy as np
 
-def main(routename):
-    authtoken = '5914f308417840fdbda37d3dc8e4b7a8'
+'''
+*Headers generator;
+'''
+def Headers_Generator(routename):
+    authtoken = '5f796921041c48dfbc1afc713416b3e8'
     global headers
     headers = {
         'authority': 'jmsgw.jtexpress.com.cn',
@@ -28,8 +31,9 @@ def main(routename):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
     }
     return headers
+
 '''
-请求头生成器
+*Time module;
 '''
 def get_time_now():
     global startDates,endDates
@@ -38,30 +42,63 @@ def get_time_now():
     endDates = str(datetime.fromtimestamp(time_now_stamp - 8400))
     print(startDates, endDates)
     return startDates,endDates
+
 '''
-时间模块
+*Package number checker and filter;
+'''
+def packageNum_check(packageNum_list):
+    routename = 'electronicPackagePrinting'
+    main(routename)
+    data = {"current":1,"size":20,"startCreateTime":"2022-07-15 00:00:00","endCreateTime":"2022-07-15 23:59:59",
+     "createNetworkCode":"0711002","packageNumberList":packageNum_list,"countryId":"1"}
+    response = requests.post('https://jmsgw.jtexpress.com.cn/customerother/electronicpackagelist/page',
+                             headers=headers,json=data).json()
+    response = response['data']['records']
+    for dic in response:
+        if dic.get('createNetworkCode') == "502701B1" or dic.get('createNetworkCode') == "502701":
+            pass
+        else:packageNum_list.remove(dic.get('packageNumber'))
+    return packageNum_list
+
+'''
+*Constuct bill code pool from system to be further handling;
 '''
 def yfwd(startDates,endDates):
     routename = 'newArriveMonitor'
     main(routename)
-    global waybillNoPool
-    waybillNoPool = []
-    pre_data = {"current":1,"size":1000,"startDates":startDates,"endDates":endDates,"siteCode":"0711002","exportType":6,"type":6,"countryId":"1"}
-    first_req = requests.post('https://jmsgw.jtexpress.com.cn/bigdataoperatingplatform/arriveMonitor/listDetail',headers=headers, json=pre_data).json()
+    temp_dic = {}
+    global packageNum_list,waybillNoPool
+    packageNum_list = []
+    waybillNo_list = []
+    pre_data = {"current":1,"size":1000,"startDates":startDates,"endDates":endDates,"siteCode":"0711002",
+                "exportType":6,"type":6,"countryId":"1"}
+    first_req = requests.post('https://jmsgw.jtexpress.com.cn/bigdataoperatingplatform/arriveMonitor/listDetail',
+                              headers=headers, json=pre_data).json()
     max_pages = first_req['data']['pages'] + 1
     for pages in range(1, max_pages):
-        data = {"current":pages,"size":1000,"startDates":startDates,"endDates":endDates,"siteCode":"0711002","exportType":6,"type":6,"countryId":"1"}
-        response = requests.post('https://jmsgw.jtexpress.com.cn/bigdataoperatingplatform/arriveMonitor/listDetail',headers=headers, json=data).json()
+        data = {"current":pages,"size":1000,"startDates":startDates,"endDates":endDates,"siteCode":"0711002",
+                "exportType":6,"type":6,"countryId":"1"}
+        response = requests.post('https://jmsgw.jtexpress.com.cn/bigdataoperatingplatform/arriveMonitor/listDetail',
+                                 headers=headers, json=data).json()
         response = response['data']['records']
         for dic in tqdm(response):
             if dic.get('packageNumber') is None:
                 pass
             elif dic.get('packageNumber')[0] == "B":
-                waybillNo = dic.get('billCode')
-                waybillNoPool.append(waybillNo)
-    return waybillNoPool
+                temp_dic['运单号'] = dic.get('billCode')
+                temp_dic['包号'] = dic.get('packageNumber')
+                packageNum_list.append(dic.get('packageNumber'))
+                waybillNo_list.append(temp_dic)
+    packageNum_check(packageNum_list)
+
+
+
+
+
+    return waybillNo_list
+
 '''
-从到件监控系统构建单号池
+*Data wash and save to xlsx file;
 '''
 def data_wash(waybillNoPool):
     routename = 'scanQueryConstantlyNew'
@@ -71,13 +108,21 @@ def data_wash(waybillNoPool):
     for i in range(count):
         ifrom = i * 200
         ito = ifrom + 200
-        pre_data = {"current": 1, "size": 1000, "startDates": "2022-07-13 00:00:00", "endDates": "2022-07-13 23:59:59","scanType": "全部", "sortName": "scanDate", "sortOrder": "desc", "bilNos": waybillNoPool[ifrom:ito],"queryTerminalDispatchCode": 0, "querySub": "", "reachAddressCodeList": [], "sendSites": [],"billType": 1, "countryId": "1"}
-        pre_req = requests.post('https://jmsgw.jtexpress.com.cn/bigdataoperatingplatform/scanRecordQuery/listPage',headers=headers, json=pre_data).json()
+        pre_data = {"current": 1, "size": 1000, "startDates": "2022-07-13 00:00:00", "endDates": "2022-07-13 23:59:59",
+                    "scanType": "全部", "sortName": "scanDate", "sortOrder": "desc", "bilNos": waybillNoPool[ifrom:ito],
+                    "queryTerminalDispatchCode": 0, "querySub": "", "reachAddressCodeList": [], "sendSites": [],
+                    "billType": 1, "countryId": "1"}
+        pre_req = requests.post('https://jmsgw.jtexpress.com.cn/bigdataoperatingplatform/scanRecordQuery/listPage',
+                                headers=headers, json=pre_data).json()
         max_pages = pre_req["data"]["pages"]
         billNoSum = pd.DataFrame()
         for pages in range(1, max_pages):
-            data = {"current": pages, "size": 1000, "startDates": "2022-07-13 00:00:00","endDates": "2022-07-13 23:59:59", "scanType": "全部", "sortName": "scanDate", "sortOrder": "desc","bilNos": waybillNoPool[ifrom:ito], "queryTerminalDispatchCode": 0, "querySub": "","reachAddressCodeList": [], "sendSites": [], "billType": 1, "countryId": "1"}
-            response = requests.post('https://jmsgw.jtexpress.com.cn/bigdataoperatingplatform/scanRecordQuery/listPage',headers=headers, json=data).json()
+            data = {"current": pages, "size": 1000, "startDates": "2022-07-13 00:00:00",
+                    "endDates": "2022-07-13 23:59:59", "scanType": "全部", "sortName": "scanDate", "sortOrder": "desc",
+                    "bilNos": waybillNoPool[ifrom:ito], "queryTerminalDispatchCode": 0, "querySub": "",
+                    "reachAddressCodeList": [], "sendSites": [], "billType": 1, "countryId": "1"}
+            response = requests.post('https://jmsgw.jtexpress.com.cn/bigdataoperatingplatform/scanRecordQuery/listPage',
+                                     headers=headers, json=data).json()
             billNoTemp = pd.DataFrame(response["data"]["records"])
             billNoTemp = billNoTemp[['billNo', 'belongNo', 'scanType', 'scanDate', 'inputDept']]
             billNoTemp.dropna(subset=['belongNo'], inplace=True)
@@ -89,20 +134,19 @@ def data_wash(waybillNoPool):
     billNoFinal.to_excel("TEST.xlsx", index=False)
     print(">>>>>>Finished!<<<<<<")
     return billNoFinal
+
 '''
-数据清洗
+*Send xlsx file via requests.post to server.
 '''
 def send_Requests():
     pass
-'''
-发送数据
-'''
+
 if __name__ == '__main__':
     while True:
         # get_time_now()
-        startDates = "2022-07-13 04:00:00"
-        endDates = "2022-07-13 04:20:00"
+        startDates = "2022-07-15 21:00:00"
+        endDates = "2022-07-21 21:10:00"
         yfwd(startDates,endDates)
-        data_wash(waybillNoPool)
+        # data_wash(waybillNoPool)
         print(">>>>>>Waiting!<<<<<<")
         time.sleep(12000)
