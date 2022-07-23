424262ca7e9fe949f1068aa4fbb6ab096d01e773
MrZhang <mrzhang_py@163.com>
2022-07-23 19:54:23 +0800

    None.

--- JTSpider/Temporary.py
+++ JTSpider/Temporary.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import copy
+from tqdm import tqdm
 
 headers = {
     'authority': 'jmsgw.jtexpress.com.cn',
@@ -21,7 +22,25 @@ headers = {
     'sec-fetch-site': 'same-site',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
 }
-
+headers2 = {
+        'authority': 'jmsgw.jtexpress.com.cn',
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'authtoken': '6abaab0aa4a3427dbc28da53256360bb',
+        'cache-control': 'max-age=2, must-revalidate',
+        'content-type': 'application/json;charset=utf-8',
+        'lang': 'zh_CN',
+        'origin': 'https://jms.jtexpress.com.cn',
+        'referer': 'https://jms.jtexpress.com.cn/',
+        'routename': 'brancTaskTrackSearchView',
+        'sec-ch-ua': '^\\^.Not/A)Brand^\\^;v=^\\^99^\\^, ^\\^Google',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '^\\^Windows^\\^',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+}
 load = {"current":1,
         "size":100,
         "startDepartureTime":"2022-07-22 10:00:00",
@@ -45,7 +64,7 @@ unload = {"current":1,
 response1 = requests.post('https://jmsgw.jtexpress.com.cn/transportation/tmsBranchTrackingDetail/page', headers=headers, json=load).json()
 response1 = response1["data"]["records"]
 list_ = []
-for dic in response1:
+for dic in tqdm(response1):
     dic_new = {}
     dic_new["运输单号"] = dic["shipmentNo"]
     dic_new["始发转运"] = dic["startName"]
@@ -54,28 +73,9 @@ for dic in response1:
     dic_new["计划发车时间"] = dic["plannedDepartureTime"]
     dic_new["实际发车时间"] = dic["actualDepartureTime"]
     dic_new["车牌号"] = dic["plateNumber"]
-    headers = {
-        'authority': 'jmsgw.jtexpress.com.cn',
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        'authtoken': '6abaab0aa4a3427dbc28da53256360bb',
-        'cache-control': 'max-age=2, must-revalidate',
-        'content-type': 'application/json;charset=utf-8',
-        'lang': 'zh_CN',
-        'origin': 'https://jms.jtexpress.com.cn',
-        'referer': 'https://jms.jtexpress.com.cn/',
-        'routename': 'brancTaskTrackSearchView',
-        'sec-ch-ua': '^\\^.Not/A)Brand^\\^;v=^\\^99^\\^, ^\\^Google',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '^\\^Windows^\\^',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-    }
     params = (('shipmentNo', dic["shipmentNo"]),)
     response2 = requests.get('https://jmsgw.jtexpress.com.cn/transportation/tmsBranchTrackingDetail/loading/scan/list',
-                            headers=headers, params=params).json()
+                            headers=headers2, params=params).json()
     dic_new["装载票数"] = response2["data"][0]["scanWaybillNum"]
     dic_new["车型"] = dic["actualVehicleTypegroup"]
     list_.append(copy.deepcopy(dic_new))
